4b332012a9c635763f225296e9198b90c662672f
beuchi123-cell <beuchi123@googlemail.com>
2026-09-11 13:37:26 +0200

    Update BestsellerResource.php

--- src/Api/Resources/BestsellerResource.php
+++ src/Api/Resources/BestsellerResource.php
@@ -2,38 +2,45 @@
 
 namespace PreisLandoBestsellerAutomation\Api\Resources;
 
-use Plenty\Plugin\ApiResource;
+use Plenty\Plugin\Controller;
+use Plenty\Plugin\Http\Response;
 use PreisLandoBestsellerAutomation\Services\BestsellerService;
 
-class BestsellerResource extends ApiResource
+class BestsellerResource extends Controller
 {
+    /** @var Response */
+    private $response;
+
+    /** @var BestsellerService */
+    private $service;
+
+    public function __construct(Response $response, BestsellerService $service)
+    {
+        $this->response = $response;
+        $this->service = $service;
+    }
+
     /**
      * Manueller Lauf entsprechend der Plugin-Einstellung "Testmodus".
      */
-    public function run()
+    public function run(): Response
     {
-        /** @var BestsellerService $service */
-        $service = pluginApp(BestsellerService::class);
-        return $this->response->json($service->run(true));
+        return $this->response->json($this->service->run(true), 200);
     }
 
     /**
      * Sicherer Testlauf: wertet aus, aendert aber niemals Tags.
      */
-    public function runTest()
+    public function runTest(): Response
     {
-        /** @var BestsellerService $service */
-        $service = pluginApp(BestsellerService::class);
-        return $this->response->json($service->run(true, true));
+        return $this->response->json($this->service->run(true, true), 200);
     }
 
     /**
      * Manueller Live-Lauf: setzt und entfernt Bestseller-Tags.
      */
-    public function runLive()
+    public function runLive(): Response
     {
-        /** @var BestsellerService $service */
-        $service = pluginApp(BestsellerService::class);
-        return $this->response->json($service->run(true, false));
+        return $this->response->json($this->service->run(true, false), 200);
     }
 }
