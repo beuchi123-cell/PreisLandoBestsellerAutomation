6bf00874b2b9d0d6bd6d4aee833f257d1ee626c4
beuchi123-cell <beuchi123@googlemail.com>
2026-09-11 12:11:48 +0200

    Update BestsellerResource.php

--- src/Api/Resources/BestsellerResource.php
+++ src/Api/Resources/BestsellerResource.php
@@ -7,10 +7,33 @@ use PreisLandoBestsellerAutomation\Services\BestsellerService;
 
 class BestsellerResource extends ApiResource
 {
+    /**
+     * Manueller Lauf entsprechend der Plugin-Einstellung "Testmodus".
+     */
     public function run()
     {
         /** @var BestsellerService $service */
         $service = pluginApp(BestsellerService::class);
         return $this->response->json($service->run(true));
+    }
+
+    /**
+     * Sicherer Testlauf: wertet aus, aendert aber niemals Tags.
+     */
+    public function runTest()
+    {
+        /** @var BestsellerService $service */
+        $service = pluginApp(BestsellerService::class);
+        return $this->response->json($service->run(true, true));
+    }
+
+    /**
+     * Manueller Live-Lauf: setzt und entfernt Bestseller-Tags.
+     */
+    public function runLive()
+    {
+        /** @var BestsellerService $service */
+        $service = pluginApp(BestsellerService::class);
+        return $this->response->json($service->run(true, false));
     }
 }
