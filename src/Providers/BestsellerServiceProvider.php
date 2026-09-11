96515edaab57783338f5cc52d7b549a9d0af3a69
beuchi123-cell <beuchi123@googlemail.com>
2026-09-11 15:45:14 +0200

    Add files via upload

--- src/Providers/BestsellerServiceProvider.php
+++ src/Providers/BestsellerServiceProvider.php
@@ -5,16 +5,22 @@ namespace PreisLandoBestsellerAutomation\Providers;
 use Plenty\Modules\Cron\Services\CronContainer;
 use Plenty\Plugin\ServiceProvider;
 use PreisLandoBestsellerAutomation\Cron\BestsellerCron;
+use PreisLandoBestsellerAutomation\Cron\BestsellerTestCron;
 
 class BestsellerServiceProvider extends ServiceProvider
 {
     public function register()
     {
-        $this->getApplication()->register(BestsellerRouteServiceProvider::class);
+        // Keine Backend-Seite und keine REST-Route noetig.
     }
 
     public function boot(CronContainer $cronContainer)
     {
+        // Fuer die normale Nutzung einmal taeglich.
         $cronContainer->add(CronContainer::DAILY, BestsellerCron::class, 0);
+
+        // Nur fuer die Inbetriebnahme. Der Handler macht nichts, solange
+        // "15-Minuten-Testlauf aktiv" in der Konfiguration auf Nein steht.
+        $cronContainer->add(CronContainer::EVERY_FIFTEEN_MINUTES, BestsellerTestCron::class, 0);
     }
 }
