9d8b704df3288c023cd3f84336e79c6251800d8e
beuchi123-cell <beuchi123@googlemail.com>
2026-09-11 13:51:37 +0200

    Update BestsellerRouteServiceProvider.php

--- src/Providers/BestsellerRouteServiceProvider.php
+++ src/Providers/BestsellerRouteServiceProvider.php
@@ -11,18 +11,23 @@ class BestsellerRouteServiceProvider extends RouteServiceProvider
     public function map(Router $router, ApiRouter $api)
     {
         $api->version(['v1'], ['middleware' => ['oauth']], function (ApiRouter $api) {
+            $api->get(
+                'preislando-bestseller/v022/ping',
+                'PreisLandoBestsellerAutomation\\Api\\Resources\\BestsellerResource@ping'
+            );
+
             $api->post(
-                'preislando-bestseller/run',
+                'preislando-bestseller/v022/run',
                 'PreisLandoBestsellerAutomation\\Api\\Resources\\BestsellerResource@run'
             );
 
             $api->post(
-                'preislando-bestseller/run-test',
+                'preislando-bestseller/v022/run-test',
                 'PreisLandoBestsellerAutomation\\Api\\Resources\\BestsellerResource@runTest'
             );
 
             $api->post(
-                'preislando-bestseller/run-live',
+                'preislando-bestseller/v022/run-live',
                 'PreisLandoBestsellerAutomation\\Api\\Resources\\BestsellerResource@runLive'
             );
         });
